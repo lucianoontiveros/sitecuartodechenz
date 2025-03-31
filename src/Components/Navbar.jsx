271cc656be4c796133308d6bf19a70def8ae7c6e
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-03-31 19:59:33 -0300

    🚀 Se realizar una actualización en el navbar para implementar botón despegable

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,46 @@
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import { useState } from "react";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const Links_elements = () => {
+    return (
+      <>
+        <Link to="/">INICIO</Link>
+        <Link to="/comandos">COMANDOS</Link>
+        <Link to="/sonidos">SONIDOS</Link>
+      </>
+    );
+  };
+
+  const toggleNavBar = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
   return (
-    <nav>
-      <div className="navbar_container">
-        <div className="logo_container">
-          <div>CUARTO DE CHENZ</div>
+    <>
+      <nav>
+        <div className="navbar_container">
+          <div className="logo_container">
+            <div>CUARTO DE CHENZ</div>
+          </div>
+          <div className="hidden link_container md:flex">
+            <Links_elements />
+          </div>
+          <div className="md:hidden">
+            <button onClick={toggleNavBar}>
+              {isOpen ? "activo" : "no activo"}
+            </button>
+          </div>
         </div>
-        <div className="link_container">
-          <Link to="/">INICIO</Link>
-          <Link to="/comandos">COMANDOS</Link>
-          <Link to="/sonidos">SONIDOS</Link>
+      </nav>
+      {isOpen && (
+        <div className="list_navbar link_container mt-3.5 text-amber-50 flex bg-amber-500">
+          <Links_elements />
         </div>
-      </div>
-    </nav>
+      )}
+    </>
   );
 };
 
