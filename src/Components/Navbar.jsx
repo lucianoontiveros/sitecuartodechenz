85572f3063d58a13941942c42da3952d5bfc1fcd
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-04-01 16:57:28 -0300

    🚀 Se realizar una actualización en el navbar para implementar botón despegable

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,43 +1,90 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import { useState } from "react";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [onList, setOnlist] = useState("hidden");
 
   const Links_elements = () => {
     return (
       <>
-        <Link to="/">INICIO</Link>
-        <Link to="/comandos">COMANDOS</Link>
-        <Link to="/sonidos">SONIDOS</Link>
+        <NavLink
+          className={({ isActive }) =>
+            `link_items ${
+              isActive ? "text-blue-500 font-bold underline" : "text-gray-500"
+            }`
+          }
+          to="/"
+          end // Esto asegura que solo esté activo en la ruta exacta
+        >
+          INICIO
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `link_items ${
+              isActive ? "text-blue-500 font-bold underline" : "text-gray-500"
+            }`
+          }
+          to="/comandos"
+        >
+          COMANDOS
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `link_items ${
+              isActive ? "text-blue-500 font-bold underline" : "text-gray-500"
+            }`
+          }
+          to="/sonidos"
+        >
+          SONIDOS
+        </NavLink>
       </>
     );
   };
 
   const toggleNavBar = () => {
     setIsOpen(!isOpen);
-    console.log(isOpen);
+    if (isOpen === false) {
+      setOnlist("contariner_links");
+    } else {
+      setOnlist("");
+    }
   };
+
+  const close_toggleNabar = () => {
+    toggleNavBar();
+  };
+
   return (
     <>
       <nav>
         <div className="navbar_container">
-          <div className="logo_container">
+          <NavLink
+            to="/"
+            className="logo_container"
+          >
             <div>CUARTO DE CHENZ</div>
-          </div>
+          </NavLink>
+
           <div className="hidden link_container md:flex">
             <Links_elements />
           </div>
-          <div className="md:hidden">
+          <div className="md:hidden toogle_navbar">
             <button onClick={toggleNavBar}>
-              {isOpen ? "activo" : "no activo"}
+              {isOpen ? "Activo" : "no activo"}
             </button>
           </div>
         </div>
       </nav>
       {isOpen && (
-        <div className="list_navbar link_container mt-3.5 text-amber-50 flex bg-amber-500">
-          <Links_elements />
+        <div className="toogle_navbar">
+          <div
+            className={onList}
+            onClick={close_toggleNabar}
+          >
+            <Links_elements />
+          </div>
         </div>
       )}
     </>
